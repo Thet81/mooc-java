fb5e9a86dab0da25f40793e7fab0a287239843b1
thetnaing <thetnaingoo1530@gmail.com>
2026-04-20 15:16:09 +0630

    fix adavancing date with parameter

--- part5/5.4/SimpleDate.java
+++ part5/5.4/SimpleDate.java
@@ -5,6 +5,9 @@ public class SimpleDate {
 	private int month;
 	private int year;
 
+	public SimpleDate(){
+		this(0,0,0);
+	}
 	public SimpleDate(int day, int month, int year) {
 		this.day = day;
 		this.month = month;
@@ -68,6 +71,34 @@ public class SimpleDate {
 		}
 
 		return false;
+	}
+
+	public void advance(){
+		this.day = this.day + 1;
+		
+		if(this.day > 30){
+			this.day = 1;
+			this.month++;
+
+			if(this.month > 12){
+				this.month = 1;
+				this.year ++;
+			}
+		}
+	}
+
+	public void advance(int howManyDays) {
+		int index = 0; 
+		while(index  < howManyDays){
+			this.advance();
+			index ++;
+		}
+	}
+
+	public SimpleDate afterNumberOfDays(int days) {
+		SimpleDate date = new SimpleDate(this.day,this.month,this.year);
+		date.advance(days);
+		return date;
 	}
 
 	@Override
