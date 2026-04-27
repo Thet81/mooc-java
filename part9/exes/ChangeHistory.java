719a7c665da03119ff0b7d8874740782fb960c4a
thetnaing <thetnaingoo1530@gmail.com>
2026-04-27 21:23:54 +0630

    done exe of change history part2

--- part9/exes/ChangeHistory.java
+++ part9/exes/ChangeHistory.java
@@ -17,6 +17,50 @@ public class ChangeHistory {
 		this.history.clear();
 	}
 
+	public double maxValue () {
+
+		if (this.history.size() == 0) {
+			return 0;
+		}
+		double maxVal = this.history.get(0);
+
+		for (double value : this.history){
+			if(value > maxVal){
+				maxVal = value;
+			}
+		}
+		return maxVal;
+	}
+
+	public double minValue () {
+		if (this.history.size() == 0) {
+			return 0;
+		}
+
+		double minVal = this.history.get(0);
+
+		for(double value : this.history){
+			if (value < minVal){
+				minVal = value;
+			}
+		}
+
+		return minVal;
+	}
+
+	public double average (){
+		if (this.history.size() == 0) {
+			return 0;
+		}
+
+		double sum = 0;
+		for(double value : this.history){
+			sum += value;
+		}
+
+		return sum / this.history.size();
+	}
+
 	@Override
 	public String toString(){
 		return this.history.toString();
--- part9/exes/ChangeHistoryDemo.java
+++ part9/exes/ChangeHistoryDemo.java
@@ -6,6 +6,14 @@ class ChangeHistoryDemo {
 		ChangeHistory history = new ChangeHistory();
 		history.add(3.0);
 		history.add(4.5);
+		history.add(7);
+		double max = history.maxValue();
+		double min = history.minValue();
+		double average = history.average();
+
 		System.out.println(history);
+		System.out.println("max value is " + max);
+		System.out.println("min value is " + min);
+		System.out.println("average is " + average);
 	}
 }
